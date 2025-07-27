2da0f9d8bba981ec658517ef50483ba1c68354c5
StevenCesen <steven.cesen@unl.edu.ec>
2025-07-27 14:35:05 -0500

    Se agregaron los EndPoint de ways y points

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PointController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WayController;
 use App\Models\Location;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,10 +12,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('users',[UserController::class,'index']);
 Route::get('users/{id}',[UserController::class,'show']);
 Route::post('users',[UserController::class,'store']);
-
-// EndPoint para autenticación
 Route::post('login',[LoginController::class,'login']);
+
 Route::get('location',function(Request $request){
     $location_create=Location::create($request->all());
     return response()->json($location_create,200);
 });
+
+Route::get('ways/',[WayController::class,'index']);
+Route::get('ways/{id}',[WayController::class,'show']);
+Route::post('ways/',[WayController::class,'store']);
+Route::put('ways/{id}',[WayController::class,'update']);
+Route::delete('ways/{id}',[WayController::class,'destroy']);
+
+Route::get('points/',[PointController::class,'index']);
+Route::post('points/',[PointController::class,'store']);
+Route::put('points/{id}',[PointController::class,'update']);
+Route::delete('points/{id}',[PointController::class,'destroy']);
